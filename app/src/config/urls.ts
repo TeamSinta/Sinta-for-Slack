4123cfad72f15c6fa9974f384e27bc4e85aeef85
msinta <shegowmo@gmail.com>
2024-06-08 19:12:40 -0400

    vercel app

--- app/src/config/urls.ts
+++ app/src/config/urls.ts
@@ -8,7 +8,7 @@
  */
 
 export const siteUrls = {
-    publicUrl: "https://5bc1e5fa5023dc7a.ngrok.app",
+    publicUrl: "https://sinta-slack.vercel.app/",
     github: "https://github.com/msinta/",
     home: "/",
     auth: {
