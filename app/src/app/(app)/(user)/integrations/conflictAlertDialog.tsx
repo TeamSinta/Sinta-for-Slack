50bab1a5c784bf94ca492ca4a8dc8f4870d357fe
msinta <shegowmo@gmail.com>
2024-07-14 19:32:16 -0400

    fixed alert dialog

--- app/src/app/(app)/(user)/integrations/conflictAlertDialog.tsx
+++ app/src/app/(app)/(user)/integrations/conflictAlertDialog.tsx
@@ -1,8 +1,9 @@
-import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogClose} from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import Image from "next/image";
 import ConflictImage from "../../../../../public/ConflictImage.png";
+import { CircleAlert } from "lucide-react";
 
 
 
@@ -10,20 +11,29 @@ export const ConflictAlertModal = () => (
     <Dialog defaultOpen={true}>
         <DialogContent>
             <DialogHeader>
-                <DialogTitle>Slack Integration Conflict</DialogTitle>
-            </DialogHeader>
+            <div className="flex items-center ">
+                        <CircleAlert className="h-6 w-6 text-red-500 mr-2" />
+                        Slack Integration Conflict
+                    </div>            </DialogHeader>
             <DialogDescription>
             <Image
                     src={ConflictImage}
                     alt="Alert"
-                    className="rounded-lg py-2"
+                    className="rounded-lg px-24 "
                 />
-                <p>It looks like this Slack team is already integrated with another organization. Please disconnect it from the other organization first.</p>
+                <p>It looks like this Slack team is already integrated with another organization. Please disconnect it from the other organization first or ask the adminstrator to join thier organization.</p>
             </DialogDescription>
             <DialogFooter>
-                <Button className="hover:bg-indigo-500 ">
-                    <Link href="/">Go to Home Page</Link>
-                </Button>
+
+          <Button variant={'outline'} >
+          <Link href="/support">Send Support Ticket</Link>
+          </Button>
+            <DialogClose asChild>
+            <Button type="button" variant={'default'} >
+              Close
+            </Button>
+
+          </DialogClose>
             </DialogFooter>
         </DialogContent>
     </Dialog>
