2cba331ebcfca899ab3c0ad789fd2392ecb017ef
msinta <shegowmo@gmail.com>
2024-08-29 15:25:09 -0400

    last change

--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -11,7 +11,7 @@ const config = {
             center: true,
             padding: "2rem",
             screens: {
-                "2xl": "1560px", // Adjust this value to be narrower than before
+                "2xl": "1400px",
             },
         },
         extend: {
