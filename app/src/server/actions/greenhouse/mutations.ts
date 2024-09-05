a2936fdbd66b1040e60b999ba1b87a5724da0958
Wubaboo <evan4217@gmail.com>
2024-09-05 11:21:02 -0400

    [analytics] Track Greenhouse Integration OK

--- app/src/server/actions/greenhouse/mutations.ts
+++ app/src/server/actions/greenhouse/mutations.ts
@@ -4,12 +4,15 @@ import { db } from "@/server/db";
 import { organizations } from "@/server/db/schema";
 import { eq } from "drizzle-orm";
 import { getOrganizations } from "../organization/queries";
+import MixpanelServer from "@/server/mixpanel";
+import { getServerAuthSession } from "@/server/auth";
 
 export async function setGreenhouseDetails(
     subDomain: string,
     API_TOKEN: string,
 ) {
     const { currentOrg } = await getOrganizations();
+    const session = await getServerAuthSession();
     const orgID = currentOrg.id;
     const result = await db
         .update(organizations)
@@ -19,5 +22,14 @@ export async function setGreenhouseDetails(
         })
         .where(eq(organizations.id, orgID))
         .execute();
+    if (result) {
+        MixpanelServer.track("Integration Connected", {
+            user_id: session?.user.id,
+            organization_id: orgID,
+            integration_id: 2,
+            integration_name: "Greenhouse",
+            integration_type: "ATS",
+        });
+    }
     return result ? "OK" : "Failed to update details";
 }
