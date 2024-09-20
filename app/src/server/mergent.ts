9bca9568f9decf06b107f90babb2ea4f9e3a46cb
Wubaboo <evan4217@gmail.com>
2024-09-20 16:04:04 -0400

    [bugfix] Await scheduling functions

--- app/src/server/mergent.ts
+++ app/src/server/mergent.ts
@@ -3,25 +3,29 @@ import Mergent from "mergent";
 // set the Mergent API key
 const mergent = new Mergent(process.env.MERGENT_API_KEY ?? "");
 
-export function scheduleTask(
+export async function scheduleTask(
     url: string,
     body: any,
     date?: Date,
     name?: string,
 ) {
-    mergent.tasks
-        .create({
+    try {
+        await mergent.tasks.create({
             request: { url, body },
             ...(date && { scheduledFor: date }),
             name,
-        })
-        .then((task) => console.log("TASK SCHEDULED SUCESSFULLY"))
-        .catch((error) => console.log("MERGENT TASK ERROR", error));
+        });
+        console.log("TASK SCHEDULED SUCCESSFULLY");
+    } catch (error) {
+        console.log("MERGENT TASK ERROR", error);
+    }
 }
 
-export function deleteTask(taskId: string) {
-    mergent.tasks
-        .delete(taskId)
-        .then((task) => console.log("TASK DELETED SUCESSFULLY"))
-        .catch((error) => console.log("MERGENT TASK ERROR", error));
+export async function deleteTask(taskId: string) {
+    try {
+        await mergent.tasks.delete(taskId);
+        console.log("TASK DELETED SUCCESSFULLY");
+    } catch (error) {
+        console.log("MERGENT TASK ERROR", error);
+    }
 }
