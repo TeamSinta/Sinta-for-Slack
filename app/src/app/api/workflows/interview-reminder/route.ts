c704e76cb41920ebc19d29f3b046d79f90d8dc83
Wubaboo <evan4217@gmail.com>
2024-09-25 17:35:52 -0400

    [refactor] Clean up interview-reminder route

--- app/src/app/api/workflows/interview-reminder/route.ts
+++ app/src/app/api/workflows/interview-reminder/route.ts
@@ -4,11 +4,8 @@ import { getSlackTeamIDByWorkflowID } from "@/server/actions/slack/query";
 import {
     fetchApplicationDetails,
     fetchCandidateDetails,
-    getCandidateJobApplication,
 } from "@/server/greenhouse/core";
 import { sendSlackNotification } from "@/server/slack/core";
-import { initializeStuckStageChecks } from "@/server/workflowTriggers/stuck-stage";
-import { customFetch } from "@/utils/fetch";
 import { NextRequest, NextResponse } from "next/server";
 
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
@@ -17,25 +14,14 @@ import { NextRequest, NextResponse } from "next/server";
 //@ts-nocheck
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 
-interface RequestBody {
-    applicationId: number;
-    lastActivity: string;
-    stageId: number;
-    stageName: string;
-    candidateId: string;
-    jobId: number;
-    jobName: string;
-}
-
 export async function POST(request: NextRequest) {
     const body = await request.json();
     // console.log("BODY", body);
-    if (!body.workflow || !body.interview) {
+    if (!body.workflow || !body.interview)
         return NextResponse.json(
             { message: "Missing required fields" },
             { status: 400 },
         );
-    }
 
     const { workflow, interview } = body;
     if (!interview.application_id)
@@ -44,36 +30,37 @@ export async function POST(request: NextRequest) {
         // Do I need to check if the interview is still scheduled?
         // If so, we have to do another API call to get the most recent interview details
 
-        // Just send the interview reminder!
-        const application = await fetchApplicationDetails(
-            interview.application_id,
-        );
-        // console.log("APPLICATION", JSON.stringify(application, null, 2));
-
-        const candidate = await fetchCandidateDetails(application.candidate_id);
-        // console.log("CANDIDATE", JSON.stringify(candidate, null, 2));
-
-        const slackTeamID = await getSlackTeamIDByWorkflowID(workflow.id);
-        const subDomain = await getSubdomainByWorkflowID(workflow.id);
-        const filteredSlackDataWithMessage = await filterCandidatesDataForSlack(
-            [candidate],
-            workflow.recipient,
-            slackTeamID,
-        );
-
-        if (filteredSlackDataWithMessage.length > 0) {
-            await sendSlackNotification(
-                filteredSlackDataWithMessage,
-                workflow.recipient,
-                slackTeamID,
-                subDomain,
-                candidate,
-            );
-        }
+        await sendSlackMessage(interview.application_id, workflow);
     } catch (e) {
         console.log("ERROR", e);
         return NextResponse.json({}, { status: 500 });
     }
 
     return NextResponse.json(body, { status: 200 });
+}
+
+async function sendSlackMessage(applicationId: string, workflow: any) {
+    const application = await fetchApplicationDetails(applicationId);
+    // console.log("APPLICATION", JSON.stringify(application, null, 2));
+
+    const candidate = await fetchCandidateDetails(application.candidate_id);
+    // console.log("CANDIDATE", JSON.stringify(candidate, null, 2));
+
+    const slackTeamID = await getSlackTeamIDByWorkflowID(workflow.id);
+    const subDomain = await getSubdomainByWorkflowID(workflow.id);
+    const filteredSlackDataWithMessage = await filterCandidatesDataForSlack(
+        [candidate],
+        workflow.recipient,
+        slackTeamID,
+    );
+
+    if (filteredSlackDataWithMessage.length > 0) {
+        await sendSlackNotification(
+            filteredSlackDataWithMessage,
+            workflow.recipient,
+            slackTeamID,
+            subDomain,
+            candidate,
+        );
+    }
 }
