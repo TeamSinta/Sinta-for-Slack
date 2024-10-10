12c2177e0441dca08fa41c64fa8b2c6f9e56ea87
msinta <shegowmo@gmail.com>
2024-10-10 18:30:02 -0400

    ts no check

--- app/src/app/api/cron/route.ts
+++ app/src/app/api/cron/route.ts
@@ -2,6 +2,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
 
 
 export const dynamic = "force-dynamic";
