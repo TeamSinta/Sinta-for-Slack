112cdeee140a8b135b4852b5916eb19873ca129d
Wubaboo <evan4217@gmail.com>
2024-10-16 13:58:21 -0400

    [feature] Add additional object fields to the candidate offer object

--- app/src/app/api/webhooks/[id]/offerCreatedHiringRooms.ts
+++ app/src/app/api/webhooks/[id]/offerCreatedHiringRooms.ts
@@ -1,13 +1,46 @@
 import { fetchHireRoomsByObjectFieldAndAlertType } from "@/server/actions/hiringrooms/queries";
 import { getSlackTeamIDByHiringroomID } from "@/server/actions/slack/query";
 import {
+    fetchApplicationDetails,
+    fetchCandidateDetails,
+    fetchJob,
+} from "@/server/greenhouse/core";
+import {
     getAttributeValue,
     initializeHiringRoomChannel,
 } from "@/utils/hiring-rooms/event-processing";
 import { checkConditions } from "@/utils/workflows";
 
 export async function handleOfferCreatedHiringRooms(data: any, orgID: string) {
-    const payload = data.payload;
+    let offer = data.payload.offer;
+    // This payload only includes data associated with offer (application, candidate, and job information must be fetched)
+
+    if (!offer?.application_id) {
+        console.log("No application ID found in payload.");
+        return;
+    }
+
+    const application = await fetchApplicationDetails(offer.application_id);
+    if (!application?.candidate_id) {
+        console.log("Could not fetch application details.", application);
+        return;
+    }
+
+    // Fetch candidate and job details concurrently
+    const candidate = await fetchCandidateDetails(application.candidate_id);
+    // Job is not used (maybe it can be used in the future)
+    // const [candidate, job] = await Promise.all([
+    //     fetchCandidateDetails(application.candidate_id),
+    //     fetchJob(application.jobs[0].id),
+    // ]);
+
+    if (!candidate) {
+        console.log("Could not fetch candidate or job details.");
+        return;
+    }
+
+    // Make the object fit the format of the candidates_attributes.json file
+    const payload = { ...offer, ...application, candidate };
 
     const hiringRooms = await fetchHireRoomsByObjectFieldAndAlertType(
         orgID,
